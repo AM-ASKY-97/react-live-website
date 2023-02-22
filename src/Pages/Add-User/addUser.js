2aab64c9832b8cbad5553ecbe451fb6d876f4402
AM-ASKY-97 <am.asky97@gmail.com>
2023-02-22 23:29:24 +0530

    first commit

---
+++ src/Pages/Add-User/addUser.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import Swal from 'sweetalert2'
+
+import Logo from './Forms-pana.svg'
+import BASE_URL from '../../App'
+
+const AddUser = () => {
+
+    const navigate = useNavigate();
+
+    const [inputs, setInputs] = useState({
+        firstName: "",
+        lastName: "",
+        contact: "",
+        email: "",
+        address: "",
+        error_list: [],
+    })
+
+    const handleChange = (event) => {
+        event.persist();
+        setInputs({ ...inputs, [event.target.name]: event.target.value })
+    }
+
+    const [photo, setPhoto] = useState([]);
+
+    const handleImage = (event) => {
+        setPhoto({ Avatar: event.target.files[0] });
+    }
+
+    const handleFormsubmit = (event) => {
+        event.preventDefault();
+
+        const formData = new FormData();
+
+        formData.append('Avatar', photo.Avatar);
+        formData.append('firstName', inputs.firstName);
+        formData.append('lastName', inputs.lastName);
+        formData.append('email', inputs.email);
+        formData.append('contact', inputs.contact);
+        formData.append('address', inputs.address)
+
+        axios.post('http://127.0.0.1:8000/api/store', formData).then(res => {
+            if (res.data.status == 200) {
+
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: res.data.message,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+
+                setInputs({
+                    firstName: "",
+                    lastName: "",
+                    contact: "",
+                    email: "",
+                });
+
+                navigate('/');
+            }
+
+            else {
+                setInputs({
+                    ...inputs, error_list: res.data.validator_error 
+                })
+            }
+        }).catch(() => {
+            Swal.fire({
+                position: 'top-end',
+                icon: 'error',
+                title: 'Oops...',
+                text: "Unable to store new user. Please check the database connection"
+            })
+        })
+    }
+
+    return (
+        <div className='container py-3'>
+
+            <div className="container pt-1">
+                <div className="row">
+                    <div className="col pt-2">
+                        <p className="h5 fw-bold">Add User</p>
+                    </div>
+                    <div className="col text-end"><Link to="/" className="btn btn-success"><i className="fa fa-chevron-circle-left" aria-hidden="true"></i> Back</Link></div>
+                </div>
+                <hr />
+            </div>
+
+            <div className='row'>
+                <div className="col-xs-12 col-sm-8 col-md-6 mt-3">
+                    <form action='' onSubmit={handleFormsubmit} encType="multipart/form-data" autocomplete="off">
+                        <div className="row mb-3">
+                            <div className="col-xs-12 col-sm-12 col-md-6 mb-3">
+                                <label htmlFor="" className="form-label">First Name</label>
+                                <input type="text" name="firstName" value={inputs.firstName} id="" className="form-control" onChange={handleChange} />
+                                <span className="text-danger mb-3" id="fName">{inputs.error_list.firstName}</span>
+                            </div>
+
+                            <div className="col-xs-12 col-sm-12 col-md-6">
+                                <label htmlFor="" className="form-label">Last Name</label>
+                                <input type="text" name="lastName" value={inputs.lastName} id="" className="form-control" onChange={handleChange} />
+                                <span className="text-danger" id="lName">{inputs.error_list.lastName}</span>
+                            </div>
+                        </div>
+
+                        <div className="row mb-3">
+                            <div className="col-xs-12 col-sm-12 col-md-6 mb-3">
+                                <label htmlFor="" className="form-label">Email</label>
+                                <input type="text" name="email" value={inputs.email} id="" className="form-control" onChange={handleChange} />
+                                <span className="text-danger" id="email">{inputs.error_list.email}</span>
+                            </div>
+
+                            <div className="col-xs-12 col-sm-12 col-md-6">
+                                <label htmlFor="" className="form-label">Phone Number </label>
+                                <input type="text" name="contact" value={inputs.contact} id="" className="form-control" onChange={handleChange} />
+                                <span className="text-danger" id="phoneNumber">{inputs.error_list.contact}</span>
+                            </div>
+                        </div>
+
+                        <div className="row mb-3">
+                            <div className="col-xs-12 col-sm-12 col-md-6 mb-3">
+                                <label htmlFor="" className="form-label">Address</label>
+                                <input type="text" name="address" value={inputs.address} id="" className="form-control" onChange={handleChange} />
+                                <span className="text-danger" id="email">{inputs.error_list.address}</span>
+                            </div>
+
+                            <div className="col-xs-12 col-sm-12 col-md-6">
+                                <label htmlFor="formFileMultiple" className="form-label">Select your profile photo</label>
+                                <input className="form-control" type="file" id="formFileMultiple" name="Avatar" onChange={handleImage} />
+                                <span className="text-danger" id="phoneNumber">{inputs.error_list.Avatar}</span>
+                            </div>
+                        </div>
+
+                        <button className='btn btn-success'><i className="fa fa-check-circle" aria-hidden="true"></i> Submit</button>
+
+                    </form>
+                </div>
+
+
+                <div className="col-sm-12 col-md-6 text-center p-3 d-none d-md-block">
+                    <img src={Logo} alt="" className="img-fluid rounded" width="400px" />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default AddUser
---
+++ src/Pages/View-User/viewUser.js
@@ -0,0 +1,160 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import Img from './img-1.jpg'
+
+import Error_page from '../Error_page/Error_page'
+
+const ViewUser = () => {
+
+    const [getValue, setValue] = useState([]);
+    const [loding, setLoding] = useState([true]);
+    const [error_loding, seterrorLoding] = useState([true]);
+    const [query, setQuery] = useState([]);
+    const [filterdata, setFilterData] = useState([]);
+
+    
+
+    const handleSearch = (event) => {
+        const getUserData = event.target.value;
+        console.log(getUserData);
+        setQuery(getUserData);
+
+        if (getUserData.length > 0) {
+            //const getUserData = event.target.value.toLowerCase();
+            //const searchData = getValue.filter(item => item.firstName.toLowerCase().includes(getUserData));
+            axios.get('http://127.0.0.1:8000/api/search/' + getUserData).then(res => {
+                if (res.data.user.length > 0) {
+                    setValue(res.data.user);
+                    document.getElementById('btn').hidden = false;
+                    document.getElementById('img').hidden = false;
+                    document.getElementById("fName").style.color = "";
+                }
+
+                else {
+                    setValue([{ "firstName": "No data Record",}])
+                    document.getElementById('btn').hidden = true;
+                    document.getElementById('img').hidden = true;
+                    document.getElementById("fName").style.color = "red";
+                }
+            })
+        }
+
+        else {
+            setValue(filterdata);
+        }
+    }
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:8000/api/index').then(res => {
+            if (res.data.status === 200) {
+                setValue(res.data.user);
+                setFilterData(res.data.user);
+                setLoding(false);
+                seterrorLoding(false);
+            }
+        }).catch(() => {
+            setLoding(false)
+        })
+    }, []);
+
+    var student_table = "";
+    var err_loding = "";
+
+    if (error_loding) {
+        if (!loding) {
+            err_loding = <Error_page />
+        }
+    }
+
+    const deleteStudent = (e, id) => {
+        const clickBtn = e.currentTarget;
+        clickBtn.innerText = "Deleting";
+
+        axios.delete('http://127.0.0.1:8000/api/destroy/' + id).then(res => {
+            clickBtn.closest("tr").remove();
+            if (res.data.status === 200) {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: res.data.message,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        })
+    }
+
+
+    return (
+        <div className='container py-3'>
+            <div className="container pt-1">
+                <div className="row">
+                    <div className="col-12 col-md-3 pb-3">
+                        <form className="d-flex" role="search">
+                            <input className="form-control me-2" type="search" value={query} placeholder="Search" aria-label="Search" onChange={(e) => handleSearch(e)} />
+                        </form>
+                    </div>
+                    <div className="col-12 col-md-9 text-end"><Link to="/add-user" className="btn btn-success"><i className="fa fa-user" aria-hidden="true"></i> Add New Contact</Link></div>
+                </div>
+                <hr />
+            </div>
+
+            <div className='table-responsive'>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Avatar</th>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Contact</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><img src={Img} width="50px" alt='Avatar' /></td>
+                            <td>Asky</td>
+                            <td>Mohammed</td>
+                            <td>0775311974</td>
+                            <td>am.asky97@gmail.com</td>
+                            <td><Link className="btn btn-primary btn-sm" style={{ marginRight: '6px' }}><i className="fa fa-search-plus" aria-hidden="true"></i> View</Link></td>
+                        </tr>
+                        {loding
+                            ? <tr className='text-center'>
+                                <td colSpan={7}>
+                                    <div className="spinner-border text-primary" role="status">
+                                        <span className="visually-hidden">Loading...</span>
+                                    </div>
+                                </td>
+                            </tr>
+                            : getValue.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td id='img'>
+                                            <img src={'http://localhost:8000/upload/students/' + item.Avatar} width="50px" alt='Avatar' />
+                                        </td>
+                                        <td id='fName'>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.contact}</td>
+                                        <td>{item.email}</td>
+                                        <td colSpan={3} className="" id='btn'>
+                                            <Link to={"ViewOneUser/" + item.id} className="btn btn-primary btn-sm" style={{ marginRight: '6px' }}><i className="fa fa-search-plus" aria-hidden="true"></i> View</Link>
+                                            <Link to={"edit_user/" + item.id} className="btn btn-warning btn-sm" style={{ marginRight: '6px' }}><i className="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</Link>
+                                            <Link onClick={(e) => deleteStudent(e, item.id)} className="btn btn-danger btn-sm"><i className="fa fa-trash-o" aria-hidden="true"></i> Delete</Link>
+                                        </td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+            {err_loding}
+        </div>
+    )
+}
+
+export default ViewUser
